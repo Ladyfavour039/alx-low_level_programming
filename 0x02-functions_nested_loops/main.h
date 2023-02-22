6ad644d958c425c602e9bad2764deb65f7e8bfd0
Ladyfavour039 <ladyf039@gmail.com>
2023-02-22 00:17:27 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int islower(int c);
+int _isapha(int c);
+int print_sing(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_baur(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
