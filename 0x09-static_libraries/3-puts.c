da905a9bba0036b4e4b149b5e7fd2691b8ef57e1
Ladyfavour039 <ladyf039@gmail.com>
2023-03-11 13:27:57 -0800

    Lib

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,18 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+* _puts - prints a string
+* @str: single character type
+*/
 
+void _puts(char *str)
+{
+int i;
+
+i = 0;
+while (str[i] != 0)
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
