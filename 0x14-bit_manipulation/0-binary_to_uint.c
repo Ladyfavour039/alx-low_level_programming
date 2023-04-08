82f0a038614f09b300803945b934f93cdfb400c0
Ladyfavour039 <Ladyf8039@gmail.com>
2023-04-08 05:54:53 -0700

    binary0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,7 +10,7 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int suv = 0, vex = 0;
+	unsigned int suv, vex;
 	int croc;
 
 	if (b == NULL)
@@ -22,7 +22,7 @@ unsigned int binary_to_uint(const char *b)
 			return (0);
 	}
 
-	for (croc = croc - 1; croc >= 0; croc--, vex *= 2)
+	for (vex = 1, suv = 0, croc--; croc >= 0; croc--, vex *= 2)
 	{
 		if (b[croc] == '1')
 			suv += vex;
