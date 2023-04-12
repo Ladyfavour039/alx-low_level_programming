88b63907a4e13a849ba941df15ff26546a515483
Ladyfavour039 <Ladyf8039@gmail.com>
2023-04-12 13:54:28 -0700

    Under_the_snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
- * Desc: Text_content A pointer to string to write to the file.
+ * @text_content: A pointer to string to write to the file.
  *
  * Return: If the function fails - -1.
  * Otherwise - 1.
@@ -11,19 +11,19 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, size = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 if (text_content != NULL)
-	{
-for (size = 0, text_content[size]
-			size++);
-	}
+{
+for (len = 0; text_content[len];)
+len++;
+}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0700);
-	w = write(o, text_content, size);
+o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0700);
+w = write(o, text_content, len);
 
 if (o == -1 || w == -1)
 return (-1);
