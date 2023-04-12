d3cb738dd34d35de0428e27d1d532c44899e04ca
Ladyfavour039 <Ladyf8039@gmail.com>
2023-04-12 12:52:30 -0700

    0-read_textfile.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,21 +11,21 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, size == 0;
+	int o, w, size = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
+if (text_content != NULL)
 	{
-		for (size = 0; text_content[size];)
-			size++;
+for (size = 0, text_content[size]
+			size++);
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0700);
 	w = write(o, text_content, size);
 
-	if (o == -1 || w == -1)
+if (o == -1 || w == -1)
 return (-1);
 
 	close(o);
