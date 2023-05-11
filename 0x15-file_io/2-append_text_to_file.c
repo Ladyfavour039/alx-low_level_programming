3896af6b1f000fc44ea692c48e4689269a3df18e
ladyfavour039 <ladyf8039@gmail.com>
2023-05-11 14:22:37 -0700

    speak_gently

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	file_write = open(filename, O_APPEND);
+	file_write = open(filename, O_WRONLY | O_APPEND);
 
 	if (file_write == -1)
 	{
